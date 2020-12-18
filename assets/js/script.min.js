628f3b7624fa3acc9d9027929b04aa5bea7c4b2f
derchris <derchris@me.com>
2020-12-19 00:48:47 +0100

    add new bootstrap layout

---
+++ assets/js/script.min.js
@@ -0,0 +1,1 @@
+$(document).ready((function(){$("[data-bs-hover-animate]").mouseenter((function(){var a=$(this);a.addClass("animated "+a.attr("data-bs-hover-animate"))})).mouseleave((function(){var a=$(this);a.removeClass("animated "+a.attr("data-bs-hover-animate"))}))}));
